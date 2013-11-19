3847d402517945353ae45d429f07a509665ac03b
isaldin <saldin.iliya@gmail.com>
2013-11-20 01:23:47 +0600

    create-method implemented for directories

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -5,4 +5,18 @@ class DirectoriesController < ApplicationController
     @items = model_class.all
   end
 
+  def new
+    @item = model_class.new
+  end
+
+  def create
+    @item = model_class.new(params[model_class.to_s.downcase.to_sym])
+
+    if @item.save
+      redirect_to boots_path, :notice => 'Successfully created'
+    else
+      render :action => :new
+    end
+  end
+
 end
